e3bb83d368004177de2c7f80048b052fd537c226
Justin <justintoeat@gmail.com>
2022-01-21 20:15:07 -0500

    Added the directional Movement
    I don't have the moving up animations yet so need that

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -8,15 +8,19 @@ public class MovementScript : MonoBehaviour
     Rigidbody2D RBody2D;
     [SerializeField] float speed = 2f;
     Vector2 motionVector;
+    Animator animator; 
 
     void Awake()
     {
         RBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         motionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate()
