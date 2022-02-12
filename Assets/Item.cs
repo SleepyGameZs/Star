8720d9fce3916378b28252b9d0fa88157c08ea19
Justin <justintoeat@gmail.com>
2022-02-12 16:06:44 -0500

    UI basics
    Its just the basic inventory UI with some more barren tiles.

---
+++ Assets/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Item")]
+
+public class Item : ScriptableObject
+{
+    public string Name;
+    public bool stackAble;
+    public Sprite icon;
+}
